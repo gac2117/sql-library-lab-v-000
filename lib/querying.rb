ddf8094ca4aa772b2ad4e3cea30a04275af81f9d
Grace Choi <oiograceoio@gmail.com>
2019-03-16 19:40:56 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,5 +20,5 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(books.title) FROM characters INNER JOIN character_books ON characters.id = character_books.character_id JOIN books ON character_books.book_id = books.id
-  GROUP BY books.id ORDER BY COUNT(books.id) DESC;"
+  GROUP BY characters.id ORDER BY COUNT(books.id) DESC;"
 end
