2e9d1f1343da1d11cb9b7ea7d4c4279ef0d49f41
Grace Choi <oiograceoio@gmail.com>
2019-03-16 18:25:01 +0000

    Automatically backed up by Learn

---
+++ lib/schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE characters(
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER,
+  series_id INTEGER
+);
+
+CREATE TABLE books(
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
+);
+
+CREATE TABLE series(
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id INTEGER,
+  subgenre_id INTEGER
+);
+
